631d3a305610c69e2b3ee9375e4d8c7fbf5de0ea
Jake Ruesink <jaruesink@gmail.com>
2020-08-28 14:59:32 -0500

    updating site with correct page title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lambda Curry Development Agency`,
+    description: `Lambda Curry is a small development agency with the technical capabilities for your custom development needs.`,
+    author: `jake@lambdacurry.dev`,
   },
   plugins: [
     {
@@ -49,7 +49,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/lc-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
